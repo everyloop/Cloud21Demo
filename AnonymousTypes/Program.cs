a89b535c27aa5d4f29ec0bb6ea505097948e9f4a
Fredrik Johansson <fredrik@everyloop.com>
2021-10-15 16:11:20 +0200

    Add projects: AnonymousTypes & LINQdemo

---
+++ AnonymousTypes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace AnonymousTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var people = new[] {
+                new { name = "Fredrik", age = 40, length = 1.80 },
+                new { name = "Maria", age = 42, length = 1.72 },
+                new { name = "Anna", age = 22, length = 1.65 },
+                new { name = "Per", age = 27, length = 1.87 }
+            };
+
+            foreach (var p in people)
+            {
+                Console.WriteLine($"{p.name} is {p.age} years old.");
+            }
+
+            Console.WriteLine();
+
+            var person = new
+            {
+                name = "Anders Eriksson",
+                age = 45,
+                contactInfo = new { eMail = "anders@gmail.com", phone = "07023485734" }
+            };
+
+            Console.WriteLine(person);
+        }
+    }
+}
---
+++ LINQdemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQdemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			var people = new[] {
+				new { firstName = "Fredrik", lastName = "Johansson", age = 40, length = 1.80, weight = 75 },
+				new { firstName = "Maria", lastName = "Andersson", age = 42, length = 1.72, weight = 72 },
+				new { firstName = "Anna", lastName = "Svensson", age = 22, length = 1.65, weight = 64 },
+				new { firstName = "Per", lastName = "Svensson", age = 27, length = 1.87, weight =  80}
+			}.ToList();
+
+			people.Add(new { firstName = "Camilla", lastName = "Karlsson", age = 67, length = 1.70, weight = 82 });
+
+			//var query = from p in people where p.age > 30 select p;
+
+			var query = from p in people group p by p.lastName;
+
+
+
+			/*
+			var newList = people
+				.Where(p => p.age > 25)
+				.Select(p => new { name = $"{p.firstName} {p.lastName}", p.age, bmi = p.weight / Math.Pow(p.length, 2) })
+				.ToList();
+
+			foreach (var person in newList)
+			{
+				Console.WriteLine($"{person.name} BMI: {person.bmi:N2}");
+			}
+			*/
+		}
+    }
+}
